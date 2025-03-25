745ab5d14c23ade3c435f238fcb88a76b8909f65
ProfiDE <abolfazl.zahedi@proton.me>
2025-03-25 20:32:31 +0330

    Added file and directory check functions

--- funcs/file-manager.funcs.php
+++ funcs/file-manager.funcs.php
@@ -1,1 +1,13 @@
+<?php
 
+class PTKFM{
+  static function IsFile(string $file_path): bool
+  {
+    return file_exists($file_path);
+  }
+
+  static function IsDir(string $dir_path): bool
+  {
+    return is_dir($dir_path);
+  }
+}
